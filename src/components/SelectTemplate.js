fbd3ee1d400920c095e2996c185f74719f0ceb50
Alberto <aaayza@gmail.com>
2022-06-09 17:27:58 -0300

    Render selects working

---
+++ src/components/SelectTemplate.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const SelectTemplate = () => {
+  return (
+    <div>
+      <select name="" id="">
+        <option value=""></option>
+      </select> 
+    </div>     
+  );
+};
+
+export default SelectTemplate;
---
+++ src/components/UsaNestedSelects.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import SelectTemplate from "./SelectTemplate";
+
+const UsaNestedSelects = () => {
+  const [state, setState] = useState("");
+  const [town, setTown] = useState("");
+  const [colony, setColony] = useState("");
+  
+  return (
+    <div>
+      <h1>Usa Nested Selects:</h1>
+      <SelectTemplate
+        title="State"
+        url=""
+        handleChange={(e) => {
+          setState(e.target.value);
+        }}
+      />
+      {state && <SelectTemplate title="town" url="" handleChange={(e) => {setTown(e.target.value);}} />}
+      {town && <SelectTemplate title="colony" url="" handleChange={(e) => {setColony(e.target.value);}} />}
+      <pre>
+        <code>
+          {state}-{town}-{colony}
+        </code>
+      </pre>
+    </div>   
+  );
+};
+
+export default UsaNestedSelects;
